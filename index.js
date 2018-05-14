8ddc8fc1925505a0ccf35a7d3a5739b35756064a
hotamisli <salihahotamisli@gmail.com>
2018-05-14 12:53:34 +0000

    message

---
+++ index.js
@@ -0,0 +1,14 @@
+/*//Function To Display Popup
+function div_show() {
+    document.getElementById('pop-up').style.display = "block";
+}
+//Function to Hide Popup
+/*function div_hide() {
+    document.getElementById('pop-up').style.display = "none";
+}*/
+
+$("#j").on('click', function() {
+    document.getElementById("pop-up").style.display = "block";
+
+
+});
